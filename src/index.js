78d71b4fbd81f291facaf2d71e0821efd0883b8c
redchicken75 <44732367+redchicken75@users.noreply.github.com>
2020-07-05 13:19:57 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+// const top10Board = document.getElementById('top10-board');
+
 var request = new XMLHttpRequest();
 request.open("GET", "http://localhost:4000/top_ten", true);
 request.onload = function () {
   var result = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
+  	var breweryRank = 0;
+  	result.data.forEach(brewery => {
+  		breweryRank += 1;
+  		var html =
+  			'<div>#' +
+  			breweryRank +
+  			'</div><div><b>Brewery Name</b>: ' +
+  			brewery.brewery_name +
+  			'</div><div><b>Average Review</b>: ' +
+  			brewery.avg_review +
+  			'</div><br />';
+  		$("#top10-board").append(html);
+  	})
   } else {
     alert("It's not working!");
   }
