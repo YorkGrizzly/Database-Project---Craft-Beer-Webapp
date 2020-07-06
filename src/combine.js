8aed78e3986ffb55567631237a372020fbadcd61
YorkGrizzly <50973240+YorkGrizzly@users.noreply.github.com>
2020-07-06 14:07:53 +0800

    Update combine.js

--- src/combine.js
+++ src/combine.js
@@ -1,13 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql');
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
 
 const app = express();
-
-const SELECT_ALL_BREWERIES = 'SELECT * FROM beer limit 5';
-const SELECT_ALL_BEERS = 'SELECT * FROM review limit 5';
-
-
 
 const TOP_TEN = `SELECT brewery.brewery_name, avg(review.review_overall) as avg_review
                 FROM brewery, review, beer
@@ -18,9 +13,9 @@ const TOP_TEN = `SELECT brewery.brewery_name, avg(review.review_overall) as avg_
                 LIMIT 10`;
 
 const RAND_GEN = `SELECT beer_name, beer_abv, beer_style 
-                  FROM beer 
-                  ORDER BY RAND() 
-                  LIMIT 1`;
+FROM beer 
+ORDER BY RAND() 
+LIMIT 1`;
 
 const SEARCH_BY_NAME = `SELECT beer.beer_name, beer.beer_abv, beer.beer_style, review.review_overall, beer.beer_id  
                         FROM beer, review
@@ -32,105 +27,81 @@ const SEARCH_BY_BREWERY = `SELECT beer.beer_name, beer.beer_abv, beer.beer_style
                             WHERE beer.brewery_id = brewery.brewery_id AND brewery.brewery_name 
                             LIKE ?`;
 
+
+
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'hello',
-    database: 'finalP'
+  host: "localhost",
+  user: "root",
+  password: "1311811Be@n",
+  database: "craftbeers",
 });
 
-
-
-connection.getConnection(function(err, connection){
-    if(err) {
-    	console.log("unable to connect")
-        return err;
-    }
+connection.getConnection(function (err, connection) {
+  if (err) {
+    console.log("unable to connect");
+    return err;
+  }
 });
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("You are at root directory of server")
+app.get("/", (req, res) => {
+  res.send("You are at root directory of server");
 });
 
-app.get('/top_ten', (req, res) => {
-    connection.query(TOP_TEN, (err, results) => {
-        if(err) {
-            return res.send(err)
-        }
-        else {
-            return res.json({
-                data: results
-            })
-        }
-    })
+app.get("/top_ten", (req, res) => {
+  connection.query(TOP_TEN, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results,
+      });
+    }
+  });
 });
 
-app.get('/gen_rand', (req, res) => {
-    connection.query(RAND_GEN, (err, results) => {
-        if(err) {
-            return res.send(err)
-        }
-        else {
-            return res.json({
-                data: results
-            })
-        }
-    })
+app.get("/gen_rand", (req, res) => {
+  connection.query(RAND_GEN, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results,
+      });
+    }
+  });
 });
 
-app.get('/search/beer_name/:query', (req, res) => {
-    let query = req.params.query;
-    query = '%' + query + '%';
-    console.log(query);
-    connection.query(SEARCH_BY_NAME, [query], (err, results) => {
-        if(err) {
-            return res.send(err)
-        }
-        else {
-            return res.json({
-                data: results
-            })
-        }
-    })
+app.get("/search/beer_name/:query", (req, res) => {
+  let query = req.params.query;
+  query = "%" + query + "%";
+  console.log(query);
+  connection.query(SEARCH_BY_NAME, [query], (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results,
+      });
+    }
+  });
 });
 
-app.get('/search/brewery_name/:query', (req, res) => {
-    let query = req.params.query;
-    query = '%' + query + '%';
-    console.log(query);
-    connection.query(SEARCH_BY_BREWERY, [query], (err, results) => {
-        if(err) {
-            return res.send(err)
-        }
-        else {
-            return res.json({
-                data: results
-            })
-        }
-    })
+app.get("/search/brewery_name/:query", (req, res) => {
+  let query = req.params.query;
+  query = "%" + query + "%";
+  console.log(query);
+  connection.query(SEARCH_BY_BREWERY, [query], (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results,
+      });
+    }
+  });
 });
-
-app.get('/add_review', (req, res) => {
-    console.log("sadkjf")
-    const {review_beer_id, review_overall, review_aroma, review_taste, review_palate, review_appearance, review_profilename} = req.query;
-    // var beer_id = req.body.add_overall;
-    console.log(review_beer_id);
-    const INSERT_BREWERIES_QUERY = `INSERT INTO review2 (beer_id, review_overall, review_time, review_aroma, review_taste, review_palate, review_appearance, review_profilename) 
-    VALUES (‘${review_beer_id}’, ‘${add_overall}’, UNIX_TIMESTAMP(), ‘${add_aroma}’, ‘${add_taste}’, ‘${add_palate}’, ‘${add_appearance}’, ‘${review_profilename}’);
-    `;
-    connection.query(INSERT_BREWERIES_QUERY, (err, results) => {
-        if (err) {
-            return res.send(err)
-        }
-        else{
-            return res.send("ran sucessfully");
-        } 
-    });
-    
-})
-
 
 // app.get('/breweries/add', (req, res) => {
 //     const {name, city, state} = req.query;
@@ -141,12 +112,11 @@ app.get('/add_review', (req, res) => {
 //         }
 //         else{
 //             return res.send("ran sucessfully");
-//         } 
+//         }
 //     });
-    
+
 // })
 
 app.listen(4000, () => {
-    console.log("The server is listening on port 4000");
-})
-
+  console.log("The server is listening on port 4000");
+});
